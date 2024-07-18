1fc8db8f1251d1ab18ebad91188b6d541b07e2c3
Yogesh Borad <31992282+yogeshborad@users.noreply.github.com>
2024-07-18 15:08:53 +0530

    Query + Routing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimetal_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
