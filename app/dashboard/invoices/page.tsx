2c9dbcc04d5e5b8b9c123442ae51bd88bd8ece82
Yogesh Borad <31992282+yogeshborad@users.noreply.github.com>
2024-07-18 19:34:31 +0530

    Pagination added

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,11 +5,15 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
  
 export default async function Page({searchParams,}:{searchParams?:{query?: string; page?: string;};}) {
 
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+
+    const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -24,7 +28,7 @@ export default async function Page({searchParams,}:{searchParams?:{query?: strin
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
